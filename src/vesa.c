acd0b5c42b504749bb6bd3f0c416fc838fc31b05
Egbert Eich <eich@suse.de>
2004-02-26 13:35:55 +0000

    readding XFree86's cvs IDs

--- src/vesa.c
+++ src/vesa.c
@@ -28,7 +28,7 @@
  * Authors: Paulo César Pereira de Andrade <pcpa@conectiva.com.br>
  *          David Dawes <dawes@xfree86.org>
  *
- * $XFree86$
+ * $XFree86: xc/programs/Xserver/hw/xfree86/drivers/vesa/vesa.c,v 1.41 2003/11/07 22:50:57 dawes Exp $
  */
 
 #include "vesa.h"
