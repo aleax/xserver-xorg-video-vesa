11f19e03e7d229b3828513d464288c4aad99d26d
Egbert Eich <eich@suse.de>
2004-02-26 09:23:28 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- src/vesa.h
+++ src/vesa.h
@@ -26,7 +26,7 @@
  *
  * Authors: Paulo César Pereira de Andrade <pcpa@conectiva.com.br>
  *
- * $XFree86: xc/programs/Xserver/hw/xfree86/drivers/vesa/vesa.h,v 1.14 2003/01/16 16:09:10 eich Exp $
+ * $XFree86$
  */
 
 #ifndef _VESA_H_
